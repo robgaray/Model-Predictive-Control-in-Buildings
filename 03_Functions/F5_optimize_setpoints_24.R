9e98328ec3fe2626b0fc7ff1758238cc44c06cee
robgaray <robgaray@gmail.com>
2026-01-12 22:23:57 +0100

    paralelization.

--- 03_Functions/F5_optimize_setpoints_24.R
+++ 03_Functions/F5_optimize_setpoints_24.R
@@ -1,4 +1,9 @@
-f5_optimize_setpoints_24 <- function(day_chunk_optimize,set_point_range_heating,set_point_range_cooling, model_parameters,Deadband,optimization_parameters) {
+f5_optimize_setpoints_24 <- function(day_chunk_optimize,
+                                     set_point_range_heating,
+                                     set_point_range_cooling,
+                                     model_parameters,
+                                     Deadband,
+                                     optimization_parameters) {
   
   horizon<- optimization_parameters$optimization_horizon
   
@@ -8,7 +13,32 @@ f5_optimize_setpoints_24 <- function(day_chunk_optimize,set_point_range_heating,
                     rep(set_point_range_cooling[1],horizon))
   upper_bounds <- c(rep(set_point_range_heating[2],horizon),
                     rep(set_point_range_cooling[2],horizon))
-
+  
+  
+  # paralelization initialization
+  {
+    n_cores <- parallel::detectCores(logical = TRUE) - 1  # leave 1 core free
+    cl <- makeCluster(n_cores)                             # works on Windows and Linux
+    registerDoParallel(cl)                                 # register cluster for foreach
+  }
+  
+  # Important for Windows (PSOCK clusters) because child processes don't inherit the global environment
+  {
+    clusterExport(
+      cl,
+      varlist = c(
+        "f4_period_calculation_adapted",
+        "day_chunk_optimize",
+        "model_parameters",
+        "Deadband",
+        "optimization_parameters",
+        "horizon"
+      ),
+      envir = environment()
+    )
+  }
+  
+  # Optimization
   ga_result <- ga(
     type = "real-valued",
     fitness = function(x) f4_period_calculation_adapted(
@@ -24,15 +54,23 @@ f5_optimize_setpoints_24 <- function(day_chunk_optimize,set_point_range_heating,
     popSize = optimization_parameters$population_size,
     maxiter = optimization_parameters$iteration_number,
     run = optimization_parameters$run_number,
+    parallel = TRUE,
     monitor = FALSE
   )
+  
+  # Stop cluster
+  stopCluster(cl)   # Stop cluster to free resources
+  registerDoSEQ()   # Return to sequential execution
+  
+  # Postprocessing
+  {
+    set_point_optimized <- ga_result@solution[1, ]
     
-  set_point_optimized <- ga_result@solution[1, ]
-  
-  set_point_optimized_heating  <- set_point_optimized[1:horizon]
-  set_point_optimized_cooling  <- set_point_optimized[(horizon+1):(2*horizon)]
-  
-  set_point_optimized<-list(set_point_optimized_heating,set_point_optimized_cooling)
+    set_point_optimized_heating  <- set_point_optimized[1:horizon]
+    set_point_optimized_cooling  <- set_point_optimized[(horizon+1):(2*horizon)]
+    
+    set_point_optimized<-list(set_point_optimized_heating,set_point_optimized_cooling)
+  }
   
   return(set_point_optimized)
 }
--- Main_SCC.R
+++ Main_SCC.R
@@ -23,255 +23,260 @@
   }
 
   # Initialisation of the file directory
-  WD<-getwd()
-
-  # Loading libraries
   {
-    ruta_librerias<- paste(WD, "/00_Libraries", sep="")  
-    .libPaths(c(ruta_librerias, .libPaths()))
-	
-    required_libraries <- c("readr","dplyr", "tidyr",
-                            "ggplot2", "zoo", "GA", "lubridate")
-
-    for (library in required_libraries) {
-        library(library,
-                character.only = TRUE,
-                lib=ruta_librerias)
-    rm(library, required_libraries)
-    }
+    WD<-getwd()
+    
+    data_path       <- file.path(WD, "01_Data")
+    config_path     <- file.path(WD, "02_config_files")
+    functions_path  <- file.path(WD, "03_Functions")
+    library_path    <- file.path(WD, "00_Libraries")
+    output_path     <- file.path(WD, "04_Output")
+    
+    main_file       <- file.path(data_path, "Main_df.rds")
+    library_file    <- file.path(config_path, "libraries.txt")
+    model_file      <- file.path(config_path, "model_parameters.csv")
+    control_file    <- file.path(config_path, "control_parameters.csv")
+	opt_file        <- file.path(config_path, "optimization_parameters.csv")
   }
-  cat("libraries loaded\n")
-
-  # Load functions
+  
+  # Loading of libraries and functions{
   {
-    files.source <- list.files(paste(WD, "/03_Functions", sep=""))
-    for (i in seq_along(files.source)) {
-      source(paste(getwd(), "/03_Functions/", files.source[i], sep=""))  
-    }
-    rm(files.source, i)    
+    source(file.path(functions_path, "initialization.R"))
+    initialization(library_file, functions_path, library_path)
+    cat("libraries loaded\n")
+    cat("functions loaded\n")
   }
-  cat("functions loaded\n")
 }
 
 #### Data & model parameters
 # no need to look inside unless you want to modify the physical model
 {
   # Load data frame
-  Main_df <- readRDS("01_Data/Main_df.rds")
+  Main_df <- readRDS(main_file)
   cat("dataframe loaded\n")
-
-  # Model parameters
-  {
-    model_parameters <- list(
-      # Building Physics
-      Ci = 1, Ce = 3.32, 
-      Rie = 0.897, Rea = 4.38,
-      Aw = 5.75, Ae = 3.87,
-      
-      # Solar control
-      Shading_0 = 0.7, Shading_1 = 1,
-      
-      # Heat Pump
-      AT_hp1 = 0.5, AT_hp2 = 1, 
-      Q_hp1 = 3, Q_hp2 = 9,
-      COP_hp1 = 3, COP_hp2 = 7/9,
-      
-      # Ventilation
-      Rvent01 = 127.15, Rvent1  = 12.72, Rvent2  = 6.36,
-      
-      # Confort bounds
-      confort_low = 21, 
-      confort_high = 26,
-      
-      # State Initialization
-      Ti_0 = 20,
-      Te_0 = 20,
-      Qh_0 = 0
-    )
-  }
-  cat("model parameters loaded\n")
   
+  # Model parameters
+  model_parameters <- load_model_parameters(model_file)
+  cat("model parameters loaded\n")
 }
 
 #### Control and optimization parameters
 # THIS IS THE PLACE TO CHANGE PARAMETERS
+# Inspect & change the parameter files OR override the parameters after they are loaded
 {
   # Setpoint parameters
   {
-    set_point_range_heating<-c(5,26)
-    Deadband <- 1
+    control <- load_control_parameters(control_file)
+    
+    set_point_range_heating <- control$set_point_range_heating
+    set_point_range_cooling <- control$set_point_range_cooling
+    Deadband                <- control$Deadband
+    
+    rm(control)
+    cat("setpoint ranges loaded\n")
   }
-  cat("setpoint bands loaded\n")
   
   # Optimization_parameters
   {
     optimization_parameters <- list(
-      population_size        = 30,
-      iteration_number       = 20,
-      run_number             = 3,
-      optimization_horizon   = 27, #hours
-      optimization_frequency = 25  #hours
+      population_size        = as.integer(cli_args[1]),
+      iteration_number       = as.integer(cli_args[2]),
+      run_number             = as.integer(cli_args[3]),
+      optimization_horizon   = as.integer(cli_args[4]), #hours
+      optimization_frequency = as.integer(cli_args[5])  #hours
     )
     
     # Corrections
     if (optimization_parameters[["optimization_frequency"]]>optimization_parameters[["optimization_horizon"]]){
       optimization_parameters[["optimization_frequency"]]<-optimization_parameters[["optimization_horizon"]]
     }
+    
+    str(optimization_parameters)
+    cat("optimization parameters loaded\n")
   }
-  str(optimization_parameters)
-  cat("optimization parameters loaded\n")
-  
+
   # subset dataframe by month
   {
-    month_subset<-1
+    month_subset<-as.integer(cli_args[6]) 
     if (month_subset!=0) {
       Main_df<-Main_df[month(Main_df$HourUTC)==month_subset,]
-      cat("Full year selected\n")
-    } else {
       cat("Month ", month_subset ," selected\n")
+    } else {
+      cat("Full year selected\n")
     }
-    Main_df$Ti[1]<-model_parameters$Ti_0
-    Main_df$Te[1]<-model_parameters$Te_0
-    Main_df$Qh[1]<-model_parameters$Qh_0
   }
 }
 
 #### Simulation
 {
-  # Get time intervals
+  # Model initialization
   {
-    optimization_timesteps<-((Main_df$HourUTC-Main_df$HourUTC[1])/(optimization_parameters[["optimization_frequency"]]*60*60))-
-      floor((Main_df$HourUTC-Main_df$HourUTC[1])/(optimization_parameters[["optimization_frequency"]]*60*60))==0
-    
-    optimization_timesteps<-Main_df$HourUTC[optimization_timesteps]
+    Main_df$Ti[1]<-model_parameters$Ti_0
+    Main_df$Te[1]<-model_parameters$Te_0
+    Main_df$Qh[1]<-model_parameters$Qh_0  
   }
-
-  # go through dataframe by steps
+  
+  # Auxiliary variables for indexing, removed at the end
+  {
+    time_sec <- as.numeric(Main_df$HourUTC)
+    t0 <- time_sec[1]
+    
+    optimization_frequency_sec <- optimization_parameters[["optimization_frequency"]] * 3600
+    optimization_horizon_sec   <- optimization_parameters[["optimization_horizon"]]   * 3600
+    
+    optimization_timesteps_idx <- which((time_sec - t0) %% optimization_frequency_sec == 0)
+    n_steps <- length(optimization_timesteps_idx)
+    
+    # Used instead of floor_date inside the loop
+    Main_df$HourUTC_hour <- as.POSIXct(format(Main_df$HourUTC, "%Y-%m-%d %H:00:00"),
+                                       tz = "UTC")
+  }
+  
+  # simulation loop
   {
     t_begin<-Sys.time()
-    n_steps <- length(optimization_timesteps)
-    
-    cat(
-      "Optimization started at time ", format(t_begin, "%Y-%m-%d %H:%M:%S"), "\n",
-      "Total timesteps: ",n_steps,"\n",
-      "Period to be optimized:\n",
-      "Begins ", format(min(Main_df$HourUTC), "%Y-%m-%d %H:%M:%S"), "\n",
-      "Ends "  , format(max(Main_df$HourUTC), "%Y-%m-%d %H:%M:%S"), "\n")
-    
+
     for (optimization_timestep in 1:n_steps)
     {
-      # Optimization horizon
-      # subset step + first timestamp in following step
+      # step definition selection
       {
-        step<-optimization_timesteps[optimization_timestep]
+        # step initiation (same for optimization and control horizons)
+        i0 <- optimization_timesteps_idx[optimization_timestep]
         
-        step_1_horizon<-step + optimization_parameters$optimization_horizon*60*60
-        step_1_horizon<-min(step_1_horizon,max(Main_df$HourUTC))
+        # optimization horizon ends
+        i_end_horizon <- max(which(time_sec <= time_sec[i0] + optimization_horizon_sec))
         
-        TF_optimize<- Main_df$HourUTC>=step & Main_df$HourUTC<=step_1_horizon
-        day_chunk_optimize<-Main_df[TF_optimize,]
+        # control horizon ends
+        i_end_control <- max(which(time_sec <= time_sec[i0] + optimization_frequency_sec))
         
-        step_1_control<-step + optimization_parameters$optimization_frequency*60*60
-        step_1_control<-min(step_1_control,max(Main_df$HourUTC))
-        
-        TF_control<-day_chunk_optimize$HourUTC>=step & day_chunk_optimize$HourUTC<=step_1_control
-        day_chunk_control<-day_chunk_optimize[TF_control,]
+        day_chunk_optimize <- Main_df[i0:i_end_horizon, ]
+        day_chunk_control  <- Main_df[i0:i_end_control, ]
       }
       
       # Verify sufficiently large step
       if (nrow(day_chunk_optimize) < 2) {
-        cat("day_chunk<2 exception case triggered\n",
-            "Optimization timestep:", optimization_timestep, "\n",
-            "Step initiation:", format(step, "%Y-%m-%d %H:%M:%S"), "\n",
-            "Step end:", format(step_1_horizon, "%Y-%m-%d %H:%M:%S"), "\n")
-        TF_merge<-Main_df$HourUTC>=step & Main_df$HourUTC<=step_1_horizon
-        Main_df[TF_merge,]<-day_chunk_optimize
+        Main_df[i0:i_end_control, ] <- day_chunk_control
         next
       }
-      
+
       # optimize setpoints
       {
-        set_point_heating_optimized <- f5_optimize_setpoints_24(day_chunk_optimize,
-                                                                set_point_range_heating,
-                                                                model_parameters,Deadband,optimization_parameters)
+        # optimization
+        set_point_optimized <- f5_optimize_setpoints_24(day_chunk_optimize,
+                                                        set_point_range_heating,set_point_range_cooling,
+                                                        model_parameters,Deadband,optimization_parameters)
         
-        set_point_actual<-as.data.frame(unique(floor_date(day_chunk_optimize$HourUTC, unit = "hour")))
-        names(set_point_actual)<-"hour"
-        set_point_actual$set_point_heating<-c(set_point_heating_optimized,0)
-        set_point_actual$set_point_heating_low <-set_point_actual$set_point_heating - Deadband/2
-        set_point_actual$set_point_heating_high<-set_point_actual$set_point_heating + Deadband/2
+        # output conversion
+        set_point_actual <- data.frame(
+          hour = unique(Main_df$HourUTC_hour[i0:i_end_horizon])
+          )
+        n_hours <- nrow(set_point_actual)
+        
+        set_point_actual$set_point_heating <- numeric(n_hours)
+        set_point_actual$set_point_cooling <- numeric(n_hours)
+        
+        n_setpoints <- length(set_point_optimized[[1]])
+        
+        if (n_setpoints > n_hours) {
+          set_point_actual$set_point_heating <- set_point_optimized[[1]][1:n_hours]
+          set_point_actual$set_point_cooling <- set_point_optimized[[2]][1:n_hours]
+        } else if (n_setpoints < n_hours) {
+          set_point_actual$set_point_heating <- c(set_point_optimized[[1]], rep(0, n_hours - n_setpoints))
+          set_point_actual$set_point_cooling <- c(set_point_optimized[[2]], rep(50, n_hours - n_setpoints))
+        } else {
+          set_point_actual$set_point_heating <- set_point_optimized[[1]]
+          set_point_actual$set_point_cooling <- set_point_optimized[[2]]
+        }
+		
+		set_point_actual$set_point_heating_low  <- set_point_actual$set_point_heating - Deadband/2
+        set_point_actual$set_point_heating_high <- set_point_actual$set_point_heating + Deadband/2
+		
+        set_point_actual$set_point_cooling_low  <- set_point_actual$set_point_cooling - Deadband/2
+        set_point_actual$set_point_cooling_high <- set_point_actual$set_point_cooling + Deadband/2
+        
       }
       
       # calculate period
       # subset step + first timestamp in following day
-      cat(
-        "Optimization timestep:", optimization_timestep, "\n",
-        "Step initiation:", format(step, "%Y-%m-%d %H:%M:%S"), "\n",
-        "Step end:", format(step_1_control, "%Y-%m-%d %H:%M:%S"), "\n",
-        "Time", format(Sys.time(), "%Y-%m-%d %H:%M:%S"), "\n")
-      
       {
         day_chunk_control <- f3_period_calculation(day_chunk_control, set_point_actual, model_parameters)
-        
-        TF_merge<-Main_df$HourUTC>=step & Main_df$HourUTC<=step_1_control
-        Main_df[TF_merge,]<-day_chunk_control
+		
+        Main_df[i0:i_end_control, ] <- day_chunk_control
       }
     }
-    
+
     t_end<-Sys.time()
     t_process <- as.numeric(difftime(t_end, t_begin, units = "secs"))
     
-    cat(
-      "Optimization ended at time ", format(t_end, "%Y-%m-%d %H:%M:%S"), "\n",
-      "Total time span: ",t_process," seconds \n")
+    Main_df$HourUTC_hour <- NULL
+    rm(time_sec, t0, optimization_frequency_sec, optimization_horizon_sec,
+       optimization_timesteps_idx, n_steps,
+       i0, i_end_horizon, i_end_control, n_hours)
+    gc()
   }
 }
 
 #### Data outputs
 {
-  if (!file.exists("04_Output_test")) {
-    dir.create("04_Output_test")
-    }
+  if (!dir.exists(output_path)) {
+    dir.create(output_path, recursive = TRUE)
+  }
 	
-  # full
-  write.csv(Main_df,
-			paste("04_Output_test/Main_df_",
-                  as.integer(cli_args[1]),"_",
-                  as.integer(cli_args[2]),"_",
-                  as.integer(cli_args[3]),"_",
-                  as.integer(cli_args[4]),"_",
-                  as.integer(cli_args[5]),"_",
-			            as.integer(cli_args[6]),".csv", sep=""))
-  write_rds(Main_df,
-            paste("04_Output_test/Main_df_",
-                  as.integer(cli_args[1]),"_",
-                  as.integer(cli_args[2]),"_",
-                  as.integer(cli_args[3]),"_",
-                  as.integer(cli_args[4]),"_",
-                  as.integer(cli_args[5]),"_",
-                  as.integer(cli_args[6]),".rds", sep=""))
-  #sinthetized
-  Sinthetized_df<-data.frame(as.data.frame(optimization_parameters),
-                             elec_heating=sum(Main_df$elec_heating),
-                             cost_heating=sum(Main_df$cost_heating),
-                             building_comfort=sum(Main_df$building_comfort),
-                             reward=sum(Main_df$reward),
-                             process_time=t_process)
-  write.csv(Sinthetized_df,
-            paste("04_Output_test/Sinthetized_df_",
-                  as.integer(cli_args[1]),"_",
-                  as.integer(cli_args[2]),"_",
-                  as.integer(cli_args[3]),"_",
-                  as.integer(cli_args[4]),"_",
-                  as.integer(cli_args[5]),"_",
-                  as.integer(cli_args[6]),".csv", sep=""))
-  write_rds(Sinthetized_df,
-            paste("04_Output_test/Sinthetized_df_",
-                  as.integer(cli_args[1]),"_",
-                  as.integer(cli_args[2]),"_",
-                  as.integer(cli_args[3]),"_",
-                  as.integer(cli_args[4]),"_",
-                  as.integer(cli_args[5]),"_",
-                  as.integer(cli_args[6]),".rds", sep=""))
-}
+  # Main_df
+  {
+    write.csv(Main_df,
+              file.path(output_path,
+                        paste("Main_df_",
+                              as.integer(cli_args[1]),"_",
+                              as.integer(cli_args[2]),"_",
+                              as.integer(cli_args[3]),"_",
+                              as.integer(cli_args[4]),"_",
+                              as.integer(cli_args[5]),"_",
+                              as.integer(cli_args[6]),
+                              ".csv",
+                              sep="")))
+    write_rds(Main_df,
+              file.path(output_path,
+                        paste("Main_df_",
+                              as.integer(cli_args[1]),"_",
+                              as.integer(cli_args[2]),"_",
+                              as.integer(cli_args[3]),"_",
+                              as.integer(cli_args[4]),"_",
+                              as.integer(cli_args[5]),"_",
+                              as.integer(cli_args[6]),
+                              ".rds",
+                              sep="")))
+  }
 
+  #sinthetized
+  {
+    Sinthetized_df<-data.frame(as.data.frame(optimization_parameters),
+                               elec_total=sum(Main_df$elec_total),
+                               elec_cost=sum(Main_df$elec_cost),
+                               building_comfort=sum(Main_df$building_comfort),
+                               reward=sum(Main_df$reward),
+                               process_time=t_process)
+    write.csv(Sinthetized_df,
+              file.path(output_path,
+                        paste("Sinthetized_df_",
+                              as.integer(cli_args[1]),"_",
+                              as.integer(cli_args[2]),"_",
+                              as.integer(cli_args[3]),"_",
+                              as.integer(cli_args[4]),"_",
+                              as.integer(cli_args[5]),"_",
+                              as.integer(cli_args[6]),
+                              ".csv",
+                              sep="")))
+    write_rds(Sinthetized_df,
+              file.path(output_path,
+                        paste("Sinthetized_df_",
+                              as.integer(cli_args[1]),"_",
+                              as.integer(cli_args[2]),"_",
+                              as.integer(cli_args[3]),"_",
+                              as.integer(cli_args[4]),"_",
+                              as.integer(cli_args[5]),"_",
+                              as.integer(cli_args[6]),
+                              ".rds",
+                              sep="")))
+  }
+}
